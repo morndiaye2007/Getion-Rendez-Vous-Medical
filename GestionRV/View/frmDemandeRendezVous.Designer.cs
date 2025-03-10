d410897be4e2e4497f9d228caf43b8fd4c837742
Mor Ndiaye <morn5574@gmail.com>
2025-03-10 01:37:34 +0000

    Modification front du projet

--- GestionRV/View/frmDemandeRendezVous.Designer.cs
+++ GestionRV/View/frmDemandeRendezVous.Designer.cs
@@ -28,12 +28,262 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnAgenda = new System.Windows.Forms.Button();
+            this.btnChoisir = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.txtNumeroOrdreMedecin = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSpecialite = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtIdentifiant = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAdresse = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNomPrenom = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgPatient = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgPatient)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(1070, 10);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(64, 26);
+            this.btnClose.TabIndex = 76;
+            this.btnClose.Text = "Fermer";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnAgenda
+            // 
+            this.btnAgenda.Location = new System.Drawing.Point(465, 10);
+            this.btnAgenda.Name = "btnAgenda";
+            this.btnAgenda.Size = new System.Drawing.Size(76, 26);
+            this.btnAgenda.TabIndex = 75;
+            this.btnAgenda.Text = "Agenda";
+            this.btnAgenda.UseVisualStyleBackColor = true;
+            // 
+            // btnChoisir
+            // 
+            this.btnChoisir.Location = new System.Drawing.Point(373, 10);
+            this.btnChoisir.Name = "btnChoisir";
+            this.btnChoisir.Size = new System.Drawing.Size(64, 26);
+            this.btnChoisir.TabIndex = 69;
+            this.btnChoisir.Text = "Choisir";
+            this.btnChoisir.UseVisualStyleBackColor = true;
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(251, 529);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(95, 26);
+            this.btnSupprimer.TabIndex = 72;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(135, 529);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(66, 26);
+            this.btnModifier.TabIndex = 70;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(19, 529);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(64, 26);
+            this.btnAjouter.TabIndex = 67;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            // 
+            // txtNumeroOrdreMedecin
+            // 
+            this.txtNumeroOrdreMedecin.Location = new System.Drawing.Point(16, 481);
+            this.txtNumeroOrdreMedecin.Name = "txtNumeroOrdreMedecin";
+            this.txtNumeroOrdreMedecin.Size = new System.Drawing.Size(166, 22);
+            this.txtNumeroOrdreMedecin.TabIndex = 66;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 450);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(145, 16);
+            this.label7.TabIndex = 74;
+            this.label7.Text = "Numero ordre medecin";
+            // 
+            // txtSpecialite
+            // 
+            this.txtSpecialite.Location = new System.Drawing.Point(16, 413);
+            this.txtSpecialite.Name = "txtSpecialite";
+            this.txtSpecialite.Size = new System.Drawing.Size(166, 22);
+            this.txtSpecialite.TabIndex = 64;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 382);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 16);
+            this.label6.TabIndex = 73;
+            this.label6.Text = "Specialite";
+            // 
+            // txtIdentifiant
+            // 
+            this.txtIdentifiant.Location = new System.Drawing.Point(16, 345);
+            this.txtIdentifiant.Name = "txtIdentifiant";
+            this.txtIdentifiant.Size = new System.Drawing.Size(166, 22);
+            this.txtIdentifiant.TabIndex = 63;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 314);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 16);
+            this.label5.TabIndex = 71;
+            this.label5.Text = "Identifiant";
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(16, 277);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(166, 22);
+            this.txtTel.TabIndex = 62;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 16);
+            this.label4.TabIndex = 68;
+            this.label4.Text = "Telephone";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(16, 209);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(166, 22);
+            this.txtEmail.TabIndex = 60;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 65;
+            this.label3.Text = "Email";
+            // 
+            // txtAdresse
+            // 
+            this.txtAdresse.Location = new System.Drawing.Point(16, 141);
+            this.txtAdresse.Name = "txtAdresse";
+            this.txtAdresse.Size = new System.Drawing.Size(166, 22);
+            this.txtAdresse.TabIndex = 59;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 61;
+            this.label2.Text = "Adresse";
+            // 
+            // txtNomPrenom
+            // 
+            this.txtNomPrenom.Location = new System.Drawing.Point(16, 73);
+            this.txtNomPrenom.Name = "txtNomPrenom";
+            this.txtNomPrenom.Size = new System.Drawing.Size(166, 22);
+            this.txtNomPrenom.TabIndex = 57;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 16);
+            this.label1.TabIndex = 58;
+            this.label1.Text = "Nom Prenom";
+            // 
+            // dgPatient
+            // 
+            this.dgPatient.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgPatient.Location = new System.Drawing.Point(373, 42);
+            this.dgPatient.Name = "dgPatient";
+            this.dgPatient.RowHeadersWidth = 51;
+            this.dgPatient.RowTemplate.Height = 24;
+            this.dgPatient.Size = new System.Drawing.Size(761, 520);
+            this.dgPatient.TabIndex = 56;
+            // 
+            // frmDemandeRendezVous
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1378, 681);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnAgenda);
+            this.Controls.Add(this.btnChoisir);
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.txtNumeroOrdreMedecin);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtSpecialite);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtIdentifiant);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAdresse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNomPrenom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgPatient);
+            this.Name = "frmDemandeRendezVous";
             this.Text = "frmDemandeRendezVous";
+            ((System.ComponentModel.ISupportInitialize)(this.dgPatient)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnAgenda;
+        private System.Windows.Forms.Button btnChoisir;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.TextBox txtNumeroOrdreMedecin;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSpecialite;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtIdentifiant;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAdresse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNomPrenom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgPatient;
     }
 }
