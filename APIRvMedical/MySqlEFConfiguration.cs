6496f39e531fb9ec9e0c41cfe634293c092fe63e
Mor Ndiaye <morn5574@gmail.com>
2025-06-25 18:36:56 +0000

    api entite soins

---
+++ APIRvMedical/MySqlEFConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using MySql.Data.EntityFramework;
+using MySql.Data.MySqlClient;
+
+namespace APIRvMedical
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))] // Important
+    public class MySqlEFConfiguration : DbConfiguration
+    {
+        public MySqlEFConfiguration()
+        {
+            SetDefaultConnectionFactory(new MySqlConnectionFactory());
+        }
+    }
+}
